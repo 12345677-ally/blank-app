94560065f072c62372749ffabff48cd26aca0d1e
12345677-ally <aisha.t.180312@gmail.com>
2026-01-30 22:44:46 +0900

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,7 +8,7 @@ from supabase import create_client, Client
 @st.cache_resource
 def init_supabase():
     url = st.secrets["supabase"]["url"]
-    key = st.secrets["supabase"]["key"]
+    key = st.secrets["supabase"]["key"]
     return create_client(url, key)
 
 supabase: Client = init_supabase()
